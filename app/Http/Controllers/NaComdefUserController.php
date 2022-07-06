5132dc3da02f937dc0e6d70fc0ab5db50874a463
Nigel <nigel.bmlt@gmail.com>
2022-07-06 22:10:56 +1000

    add controllers

---
+++ app/Http/Controllers/NaComdefUserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NaComdefUser;
+use Illuminate\Http\Request;
+
+class NaComdefUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NaComdefUser  $naComdefUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NaComdefUser $naComdefUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NaComdefUser  $naComdefUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NaComdefUser $naComdefUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NaComdefUser  $naComdefUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NaComdefUser $naComdefUser)
+    {
+        //
+    }
+}
